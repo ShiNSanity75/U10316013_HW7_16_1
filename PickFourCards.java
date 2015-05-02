5cd5592c4ef6ab686e109f7e60a48e62ebf9b394
ShiNSanity75 <jj06280705@gmail.com>
2015-05-02 17:02:36 +0800

    視窗內容架構完成

--- PickFourCards.java
+++ PickFourCards.java
@@ -3,6 +3,18 @@ import javax.swing.*;
 
 public class PickFourCards extends JFrame {
 	public PickFourCards() {
+		//Create panel p1 to display the four cards
+		JPanel p1 = new JPanel();
+		p1.setLayout(new GridLayout(1, 4));
+		
+		//Create panel p2 to put the refresh button
+		JPanel p2 = new JPanel();
+		p2.setLayout(new GridLayout(1, 1));
+		p2.add(new JButton("Refresh"));
+		
+		//Add contents to the frame
+		add(p1, BorderLayout.NORTH);
+		add(p2, BorderLayout.SOUTH);
 		
 	}
 	
@@ -15,3 +27,4 @@ public class PickFourCards extends JFrame {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true); //Display the frame
 	}
+}
