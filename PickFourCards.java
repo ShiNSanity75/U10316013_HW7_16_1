b8a7163a11c90a6b8a66725217dfe5b8790f014a
ShiNSanity75 <jj06280705@gmail.com>
2015-05-03 21:59:48 +0800

    改大小

--- PickFourCards.java
+++ PickFourCards.java
@@ -74,7 +74,7 @@ public class PickFourCards extends JFrame {
 	public static void main (String[] args) {
 		PickFourCards frame = new PickFourCards(); //Create a frame
 		frame.setTitle("Pick Four Cards"); //set the title of frame
-		frame.setSize(400, 300); //Set the size of frame
+		frame.setSize(300, 200); //Set the size of frame
 		frame.setLocationRelativeTo(null); //Center the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true); //Display the frame
